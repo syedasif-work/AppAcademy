6c204fd6f33b64875823a23cc9d4fb2edfda586a
Syed Asif <syedasn@amazon.com>
2022-07-01 20:22:56 +0530

    Practice: Inheritance Completed

---
+++ practice-for-week-04-inheritance-main/classes/teacher.js
@@ -0,0 +1,27 @@
+const Person = require('./person');
+
+// Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    let totalExperience = 0;
+    for (let teacher of teachers) {
+      totalExperience += teacher.yearsOfExperience;
+    }
+    return totalExperience;
+  }
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Teacher;
+} catch {
+  module.exports = null;
+}
