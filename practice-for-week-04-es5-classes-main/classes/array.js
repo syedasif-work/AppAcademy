b6e6f2e745b481bb3d69a567ce05ef10de36c42c
Syed Asif <syedasn@amazon.com>
2022-07-04 21:37:45 +0530

    Practice: ES5 Class Syntax

---
+++ practice-for-week-04-es5-classes-main/classes/array.js
@@ -0,0 +1,10 @@
+// Your code here
+
+Array.prototype.isEqual = function(array) {
+    for (let i = 0; i < this.length; i++){
+        if (this[i] != array[i]){
+            return false;
+        }
+    }
+    return true;
+}
