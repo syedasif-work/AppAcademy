ecc6f547287d273f77ee6263713853574fb79673
Syed Asif <syedasn@amazon.com>
2022-07-04 20:15:07 +0530

    Long Practice: Context Completed

---
+++ practice-for-week-04-context-long-practice-main/problems/07-all-the-args.js
@@ -0,0 +1,32 @@
+function allTheArgs(func, ...args) {
+  // Your code here
+  return func.bind(this, ...args);
+}
+
+function sum(...nums) {
+  return nums.reduce((acc, num) => acc + num);
+}
+
+const onePlusTwo = allTheArgs(sum, 1, 2);
+
+const onePlusTwoPlusThree = onePlusTwo(3);
+const onePlusTwoPlusFour = onePlusTwo(4);
+
+console.log(onePlusTwoPlusThree); // => 6
+console.log(onePlusTwoPlusFour);  // => 7
+
+const bow = (...names) => {
+  let nameArr = Array.from(names);
+  return "You bowed to " + names.join(" and ");
+};
+
+console.log(bow("Sandy")) // prints "You bowed to Sandy"
+
+let bowSandy = allTheArgs(bow, "Sandy");
+console.log(bowSandy()); // prints "You bowed to Sandy"
+console.log(bowSandy("Joe", "Nico")); // prints "You bowed to Sandy and Joe and Nico"
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+module.exports = allTheArgs;
---
+++ practice-for-week-04-context-long-practice-main/problems/08-call-me-later.js
@@ -0,0 +1,21 @@
+// Your code here
+class CallCenter{
+	constructor(name) {
+		this.name = name;
+	}
+
+	sayHello = () => {
+		console.log(`Hello this is ${this.name}`);
+	}
+
+	callMeLater = (delay) => setTimeout(this.sayHello, delay);
+}
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+try {
+	module.exports = CallCenter;
+} catch {
+	module.exports = null;
+}
